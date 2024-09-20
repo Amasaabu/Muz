ebc6029120d734d1fa50234bde8aef1c9a6e26ef
Abubakar Amasa <amasaabubaakr@gmail.com>
2024-09-20 16:04:37 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
-import './App.css';
-
+import stlye from './app.module.css';
+import { useState } from 'react';
 function App() {
+
+  const [formdata, setFormData]= useState({
+    privateKey: {value: '', error: '', name: 'PrivateKey'},
+    inputMint: {value: '', error: '', name: 'InputMint'},
+    outputMint: {value: '', error: '', name: 'Output'},
+    amount: {value: '', error: '', name: 'Amount'},
+    blockChain: {value: '', error: '', name: 'BlockChain'},
+  })
+
+  const Submit=(e)=>{
+    e.preventDefault()
+      console.log(formdata.privateKey.value)
+      console.log(formdata.inputMint.value)
+      console.log(formdata.outputMint.value)
+      console.log(formdata.amount.value)
+      console.log(formdata.blockChain.value)
+  }
+  const form = Object.keys(formdata).map((key)=>{
+    return (
+      <div>
+      <label htmlFor={formdata[key].name}>{formdata[key].name}</label>
+      <input onChange={(e)=>{
+        setFormData({
+          ...formdata,
+          [key]:{
+            ...formdata[key],
+            value: e.target.value
+          }
+        })
+      }}
+      
+      value={formdata[key].value} type="text" placeholder={formdata[key].name} />
+    </div>
+    )
+  }) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Form</h1>
+      <form className={stlye.form}>
+        {form}
+      </form>
+      <button onClick={Submit}>Submit</button>
     </div>
   );
 }
