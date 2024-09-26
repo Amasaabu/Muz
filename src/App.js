f1535b183242605611fa29909061c3c01ea5e083
Abubakar Amasa <amasaabubaakr@gmail.com>
2024-09-26 14:33:15 +0100

    edit&delete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import stlye from './app.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
-
+  const [curr, setCurr] = useState([])
   const [formdata, setFormData]= useState({
     privateKey: {value: '', error: '', name: 'PrivateKey: '},
     inputMint: {value: '', error: '', name: 'InputMint:'},
@@ -10,14 +10,33 @@ function App() {
     amount: {value: '', error: '', name: 'Amount:'},
     blockChain: {value: '', error: '', name: 'BlockChain:'},
   })
+  const load = async()=>{
+    //load databaase state
+    const {data} = await axios.get('http://localhost:3001/get')
+    console.log(data)
+    setCurr(data)
+  }
+
+  const del = async(id)=>{
+    const {data} = await axios.delete(`http://localhost:3001/delete/${id}`) 
+    console.log("After deleting", data)
+    //after deleting reload data
+    load()
+  }
+  useEffect(()=>{
+    load()
+  }, [])
 
   const Submit=(e)=>{
     e.preventDefault()
-      console.log(formdata.privateKey.value)
-      console.log(formdata.inputMint.value)
-      console.log(formdata.outputMint.value)
-      console.log(formdata.amount.value)
-      console.log(formdata.blockChain.value)
+    const response = axios.post('http://localhost:3001/add', {
+      privateKey: formdata.privateKey.value,
+      inputMint: formdata.inputMint.value,
+      outputMint: formdata.outputMint.value,
+      amount: formdata.amount.value,
+      blockChain: formdata.blockChain.value
+    })
+    load()
   }
   const form = Object.keys(formdata).map((key)=>{
     return (
@@ -44,6 +63,22 @@ function App() {
         {form}
       </form>
       <button onClick={Submit}>Submit</button>
+      <h2>Current State</h2>
+      {curr.length&&curr.map((item, key)=>{
+        console.log(item)
+        return (
+        <table key={key}>
+          <tr>
+            <td>{item.privateKey}</td>
+            <td>{item.inputMint}</td>
+            <td>{item.outputMint}</td>
+            <td>{item.amount}</td>
+            <td>{item.blockChain}</td>
+            <span className={stlye.action} style={{margin: "40px"}}>Edit</span>
+            <span onClick={()=>del(item.id)} className={stlye.action}>Delete</span>
+            </tr>
+        </table>)
+      })}
     </div>
   );
 }
